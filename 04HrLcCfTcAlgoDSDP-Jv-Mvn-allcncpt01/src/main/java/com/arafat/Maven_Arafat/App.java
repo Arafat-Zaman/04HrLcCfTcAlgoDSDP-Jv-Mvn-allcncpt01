f2fe55bce4dc07b11f412c9443de03f678cff3ea
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2021-07-06 06:22:56 +0600

    Mvn

---
+++ 04HrLcCfTcAlgoDSDP-Jv-Mvn-allcncpt01/src/main/java/com/arafat/Maven_Arafat/App.java
@@ -0,0 +1,13 @@
+package com.arafat.Maven_Arafat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
